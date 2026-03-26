ac097ac17566fcd6ffe430611777dc122519704c
codeby-rafay <rafayali146@gmail.com>
2026-03-26 17:05:06 +0500

    first commit

---
+++ Currency-Converter-Using-Api/script.js
@@ -0,0 +1,60 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newoption = document.createElement("option");
+    newoption.innerText = currCode;
+    newoption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newoption.selected = "selected";
+    } else if (select.name === "to" && currCode === "PKR") {
+      newoption.selected = "selected";
+    }
+    select.append(newoption);
+  }
+  select.addEventListener("change", (e) => {
+    updateFlag(e.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+
+  let finalAmount = amtVal * rate;
+
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
